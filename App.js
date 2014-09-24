14457dfd6f15e4a2977c5702ff1b625e00886c5d
N M <nmusaelian@rallydev.com>
2014-09-24 13:54:10 -0600

    firt commit

---
+++ App.js
@@ -0,0 +1,89 @@
+        Ext.define('CustomApp', {               
+            extend: 'Rally.app.App',
+            componentCls: 'app',
+            _newObj : {},
+            childrens: [],
+            _type : null,
+            launch: function() {
+                Ext.create('Rally.ui.dialog.ChooserDialog', {
+                    width: 450,
+                    autoScroll: true,
+                    height: 525,
+                    title: 'Select to Copy',
+                    pageSize: 100,
+                    closable: false,
+                    selectionButtonText: 'Copy',                  
+                    artifactTypes: ['PortfolioItem/Feature','PortfolioItem/Initiative','PortfolioItem/Theme'],
+                    autoShow: true,
+                    storeConfig:{
+                        fetch: ['Name','PortfolioItemTypeName','Children'] //added 'Children' thinking that may fix Uncaught TypeError: Cannot read property 'isCollection' of undefined
+                        //https://rally1.rallydev.com/slm/doc/webservice/ there is no Children on Features, only on Initiative and Theme
+                    },
+                    listeners: {
+                        artifactChosen: function(selectedRecord) {
+                            childrens = [];
+                            this._type = selectedRecord.get('PortfolioItemTypeName');
+                            this._newObj = selectedRecord;
+                            console.log('this._newObj.data.Children.Count:',this._newObj.data.Children.Count );
+                            //this.onqModelRetrieved(); //does not exist
+                            var self = this;
+                            Ext.create('Rally.data.wsapi.Store', {
+                                model: 'PortfolioItem/' + selectedRecord.get('PortfolioItemTypeName'),
+                                fetch: ['Name', 'FormattedID', 'Children'],
+                                pageSize: 1,
+                                autoLoad: true,
+                                listeners: {
+                                    load: function(store, records) {
+                                        final_features = [];
+                                        Ext.Array.each(records, function(child){
+                                            var item = selectedRecord;
+                                            childrens = item.getCollection('Children');
+                                            childrens.load({
+                                                fetch: ['FormattedID'],
+                                                callback: function(records, operation, success){
+                                                    Ext.Array.each(records, function(portfolioitem){
+                                                        if (portfolioitem.get('PortfolioItemTypeName') == "Feature") {
+                                                            self._childObj = portfolioitem;
+                                                            self._copyChild();
+                                                        }   
+                                                    }, self);   
+                                                },
+                                                scope: this 
+                                            });     
+                                        }, self);
+                                    }   
+                                }
+                            });
+                        },
+                        scope: this
+                    },
+                }); 
+            },
+            // Inner Copy functions
+            _copyChild: function() {
+                var that = this;
+                console.log("in  _copyChild, that._childObj.data.FormattedID:", that._childObj.data.FormattedID); //Uncaught ReferenceError: that is not defined
+                this.innerModelRetrieved();
+            },
+            innerModelRetrieved: function() {
+                var that = this;
+                (function(){
+                    var child = that._childObj;
+                    console.log("in innerModelRetrieved, that._childObj.data.FormattedID:", that._childObj.data.FormattedID);
+                    that._type = 'PortfolioItem/' + that._childObj.get('PortfolioItemTypeName');
+                    Rally.data.ModelFactory.getModel({
+                        type: that._type,
+                        success: function(model){
+                        that.onInnerModelRetrieved(model, child );
+                    },
+                    scope: that
+                });
+                })();
+                
+            },                      
+            onInnerModelRetrieved: function(model, _childObj ) {
+                console.log("in onInnerModelRetrieved, that._childObj.data.FormattedID:", _childObj.data.FormattedID);
+                this.model = model;
+                //this.genericInnerCopy(model); //Uncaught TypeError: undefined is not a function 
+            },
+});
